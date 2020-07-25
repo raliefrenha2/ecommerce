605ce1054ecde1cf93a3cce0fc80d23eb2bf8698
Romi Alief Rahman <raliefrenha2@gmail.com>
2020-07-25 10:59:02 +0700

    Filter & Product Detail

--- app/Http/Controllers/Ecommerce/FrontController.php
+++ app/Http/Controllers/Ecommerce/FrontController.php
@@ -23,8 +23,25 @@ class FrontController extends Controller
 	    //BUAT QUERY UNTUK MENGAMBIL DATA PRODUK, LOAD PER PAGENYA KITA GUNAKAN 12 AGAR PRESISI PADA HALAMAN TERSEBUT KARENA DALAM SEBARIS MEMUAT 4 BUAH PRODUK
 	    $products = Product::orderBy('created_at', 'DESC')->paginate(12);
 	    //LOAD JUGA DATA KATEGORI YANG AKAN DITAMPILKAN PADA SIDEBAR
-	    $categories = Category::with(['child'])->withCount(['child'])->getParent()->orderBy('name', 'ASC')->get();
+	    // $categories = Category::with(['child'])->withCount(['child'])->getParent()->orderBy('name', 'ASC')->get(); //-> dipindah ke View Composer
 	    //LOAD VIEW PRODUCT.BLADE.PHP DAN PASSING KEDUA DATA DIATAS
-	    return view('ecommerce.product', compact('products', 'categories'));
+	    return view('ecommerce.product', compact('products'));
+	}
+
+	 public function categoryProduct($slug)
+	{
+	   //JADI QUERYNYA ADALAH KITA CARI DULU KATEGORI BERDASARKAN SLUG, SETELAH DATANYA DITEMUKAN
+	   //MAKA SLUG AKAN MENGAMBIL DATA PRODUCT YANG BERELASI MENGGUNAKAN METHOD PRODUCT() YANG TELAH DIDEFINISIKAN PADA FILE CATEGORY.PHP SERTA DIURUTKAN BERDASARKAN CREATED_AT DAN DI-LOAD 12 DATA PER SEKALI LOAD
+	    $products = Category::where('slug', $slug)->first()->product()->orderBy('created_at', 'DESC')->paginate(12);
+	    //LOAD VIEW YANG SAMA YAKNI PRODUCT.BLADE.PHP KARENA TAMPILANNYA AKAN KITA BUAT SAMA JUGA
+	    return view('ecommerce.product', compact('products'));
+	}
+
+	public function show($slug)
+	{
+	    //QUERY UNTUK MENGAMBIL SINGLE DATA BERDASARKAN SLUG-NYA
+	    $product = Product::with(['category'])->where('slug', $slug)->first();
+	    //LOAD VIEW SHOW.BLADE.PHP DAN PASSING DATA PRODUCT
+	    return view('ecommerce.show', compact('product'));
 	}
 }
