26b449eec5ecfab12f0d36090a3f5871b666a23b
Romi Alief Rahman <raliefrenha2@gmail.com>
2020-07-25 07:34:21 +0700

    Fitur Category

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,6 +20,7 @@ class RedirectIfAuthenticated
     {
         if (Auth::guard($guard)->check()) {
             return redirect(RouteServiceProvider::HOME);
+            // return redirect('/administrator/home'); //UBAH BAGIAN INI
         }
 
         return $next($request);
