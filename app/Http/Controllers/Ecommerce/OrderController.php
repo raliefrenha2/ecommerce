b4c01838b2c4e5e3f548332fe27d5abf2040cb12
Romi Alief Rahman <raliefrenha2@gmail.com>
2020-10-03 15:52:34 +0700

    How to Use Laravel Authorization

--- app/Http/Controllers/Ecommerce/OrderController.php
+++ app/Http/Controllers/Ecommerce/OrderController.php
@@ -20,8 +20,17 @@ class OrderController extends Controller
 	public function view($invoice)
 	{
 	    $order = Order::with(['district.city.province', 'details', 'details.product', 'payment'])
-	        ->where('invoice', $invoice)->first();
-	    return view('ecommerce.orders.view', compact('order'));
+			->where('invoice', $invoice)->first();
+				
+		//JADI KITA CEK, VALUE forUser() NYA ADALAH CUSTOMER YANG SEDANG LOGIN
+		//DAN ALLOW NYA MEMINTA DUA PARAMETER
+		//PERTAMA ADALAH NAMA GATE YANG DIBUAT SEBELUMNYA DAN YANG KEDUA ADALAH DATA ORDER DARI QUERY DI ATAS
+		if (\Gate::forUser(auth()->guard('customer')->user())->allows('order-view', $order)) {
+			//JIKA HASILNYA TRUE, MAKA KITA TAMPILKAN DATANYA
+			return view('ecommerce.orders.view', compact('order'));
+		}
+		//JIKA FALSE, MAKA REDIRECT KE HALAMAN YANG DIINGINKAN
+		return redirect(route('customer.orders'))->with(['error' => 'Anda Tidak Diizinkan Untuk Mengakses Order Orang Lain']);
 	}
 
 	public function paymentForm()
